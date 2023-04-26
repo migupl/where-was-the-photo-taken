96e0abe08a38abb081d1c79a2ef6b02a9ceec14a
Miguel Pagola <mipagola@gmail.com>
2023-04-26 12:07:54 +0200

    refactor: extract to method

--- js/photo-album.js
+++ js/photo-album.js
@@ -1,16 +1,19 @@
 import { GeoJSONFeatures } from './geojson-features.js';
 
+const addPointToMap = point => {
+    const map = document.querySelector('leaflet-map');
+    const eventBus = map.eventBus;
+
+    eventBus.dispatch('x-leaflet-map-geojson-add', { leafletMap: map, geojson: point })
+}
+
 document.addEventListener('drop-photo-for-exif:data', (event) => {
     event.preventDefault();
 
     const data = event.detail;
     if (data.location) {
         const point = GeoJSONFeatures.getGeoJSONPoint(data);
-
-        const map = document.querySelector('leaflet-map');
-        const eventBus = map.eventBus;
-
-        eventBus.dispatch('x-leaflet-map-geojson-add', { leafletMap: map, geojson: point })
+        addPointToMap(point);
     }
     else {
         alert(`The added photo '${data.name}' has no geolocation data`);
