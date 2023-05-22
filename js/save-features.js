15c72a2a65b6f8acb8a1e271f3106e8e337d5072
Miguel Pagola <mipagola@gmail.com>
2023-05-22 13:11:56 +0200

    refactor: removing unnecessary

--- js/save-features.js
+++ js/save-features.js
@@ -2,12 +2,6 @@ import { ZIP } from './deps/zip-streams.js'
 
 class SaveFeatures {
 
-    #supportsApi;
-
-    constructor() {
-        this.#supportsApi = this.#allowsFileSystemAccess();
-    }
-
     toZipFile = (features, images, suggestedName = 'photos.zip') => {
         const filename = suggestedName.endsWith('.zip') ? suggestedName : (suggestedName + '.zip');
         const name = filename.split('.').slice(0, -1).join('.');
@@ -15,20 +9,6 @@ class SaveFeatures {
         const geojsonFile = this.#getGeojsonFile(features, name);
 
         this.#saveZipFile(geojsonFile, images, filename);
-
-        this.#saveGeoJsonFile(geojsonFile);
-        this.#saveImages(images);
-    }
-
-    #allowsFileSystemAccess = () => {
-        return 'showSaveFilePicker' in window &&
-            (() => {
-                try {
-                    return window.self === window.top;
-                } catch {
-                    return false;
-                }
-            })();
     }
 
     #getGeojsonFile(features, name) {
@@ -45,57 +25,6 @@ class SaveFeatures {
             type: blob.type,
         });
         return file;
-    }
-
-    #saveGeoJsonFile = (file) => {
-        this.#saveFile(file, file.name);
-    }
-
-    #saveFile = (blob, filename) => {
-        if (this.#supportsApi) {
-            this.#saveFileUsingFSAApi(blob, filename);
-        }
-        else {
-            this.#saveFileWithoutFSAApi(blob, filename);
-        }
-    }
-
-    #saveFileWithoutFSAApi = (blob, filename) => {
-        const blobURL = URL.createObjectURL(blob);
-        const a = document.createElement('a');
-        a.href = blobURL;
-        a.download = filename;
-        a.style.display = 'none';
-        document.body.append(a);
-        a.click();
-
-        setTimeout(() => {
-            URL.revokeObjectURL(blobURL);
-            a.remove();
-        }, 1000);
-    }
-
-    #saveFileUsingFSAApi = async (blob, filename) => {
-        try {
-            const handle = await showSaveFilePicker({
-                suggestedName: filename,
-            });
-            const writable = await handle.createWritable();
-            await writable.write(blob);
-            await writable.close();
-            return;
-        } catch (err) {
-            if (err.name !== 'AbortError') {
-                console.error(err.name, err.message);
-                return;
-            }
-        }
-    }
-
-    #saveImages = images => {
-        images.forEach(async (image) => {
-            await this.#saveFileWithoutFSAApi(image, image.name);
-        });
     }
 
     #saveZipFile = (geojsonFile, images, filename) => {
