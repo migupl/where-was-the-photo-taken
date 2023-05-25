214cba77d3fe39d6d43c71683fbdbde1f8a5a398
Miguel Pagola <mipagola@gmail.com>
2023-05-25 11:58:27 +0200

    feat: include all on window load

--- js/photo-album.js
+++ js/photo-album.js
@@ -1,74 +1,74 @@
 import { GeoJSONFeatures } from './geojson-features.js';
 
-const existingPoints = new Set();;
-const addPointToMap = point => {
-    const { id, geojson } = point;
-    if (!existingPoints.has(id)) {
-        const map = document.querySelector('leaflet-map');
-        const eventBus = map.eventBus;
-
-        eventBus.dispatch('x-leaflet-map-geojson-add', { leafletMap: map, geojson: geojson });
-        existingPoints.add(id);
-    }
-}
-
-document.addEventListener('drop-photo-for-exif:image', (event) => {
-    event.stopPropagation();
-
-    const data = event.detail;
-    if (data.location) {
-        GeoJSONFeatures.addPhoto(data);
-    }
-    else {
-        alert(`The added photo '${data.name}' has no geolocation data`);
-    }
-});
-
-document.addEventListener('drop-photo-for-exif:file', (event) => {
-    event.stopPropagation();
-
-    const file = event.detail;
-    const refreshTitle = title => {
-        const eTitle = document.getElementById('title');
-        eTitle.value = title;
-    }
-
-    if (GeoJSONFeatures.isGeojson(file)) GeoJSONFeatures.add(file, refreshTitle);
-});
-
-document.addEventListener('drop-photo-for-exif:completed-batch', (event) => {
-    event.stopPropagation();
-
-    const points = GeoJSONFeatures.getGeoJSONPoints();
-    points.forEach(addPointToMap);
-
-    toggleSaveButton();
-});
-
-document.addEventListener('x-leaflet-map:marker-removed', (event) => {
-    event.stopPropagation();
-
-    const { feature } = event.detail;
-    const { card } = feature.data;
-
-    existingPoints.delete(card.id);
-    toggleSaveButton();
-
-    GeoJSONFeatures.remove(card);
-})
-
-
-const save = document.getElementById('save-all');
-const toggleSaveButton = () => save.style.display = existingPoints.size > 0 ? 'block' : 'none';
-
-toggleSaveButton();
-save.addEventListener('click', (event) => {
-    event.stopPropagation();
-    const title = document.getElementById('title').value || document.getElementById('title').placeholder
-    GeoJSONFeatures.saveAllPoints(title);
-});
-
 window.onload = () => {
+    const existingPoints = new Set();;
+    const addPointToMap = point => {
+        const { id, geojson } = point;
+        if (!existingPoints.has(id)) {
+            const map = document.querySelector('leaflet-map');
+            const eventBus = map.eventBus;
+
+            eventBus.dispatch('x-leaflet-map-geojson-add', { leafletMap: map, geojson: geojson });
+            existingPoints.add(id);
+        }
+    }
+
+    document.addEventListener('drop-photo-for-exif:image', (event) => {
+        event.stopPropagation();
+
+        const data = event.detail;
+        if (data.location) {
+            GeoJSONFeatures.addPhoto(data);
+        }
+        else {
+            alert(`The added photo '${data.name}' has no geolocation data`);
+        }
+    });
+
+    document.addEventListener('drop-photo-for-exif:file', (event) => {
+        event.stopPropagation();
+
+        const file = event.detail;
+        const refreshTitle = title => {
+            const eTitle = document.getElementById('title');
+            eTitle.value = title;
+        }
+
+        if (GeoJSONFeatures.isGeojson(file)) GeoJSONFeatures.add(file, refreshTitle);
+    });
+
+    document.addEventListener('drop-photo-for-exif:completed-batch', (event) => {
+        event.stopPropagation();
+
+        const points = GeoJSONFeatures.getGeoJSONPoints();
+        points.forEach(addPointToMap);
+
+        toggleSaveButton();
+    });
+
+    document.addEventListener('x-leaflet-map:marker-removed', (event) => {
+        event.stopPropagation();
+
+        const { feature } = event.detail;
+        const { card } = feature.data;
+
+        existingPoints.delete(card.id);
+        toggleSaveButton();
+
+        GeoJSONFeatures.remove(card);
+    })
+
+
+    const save = document.getElementById('save-all');
+    const toggleSaveButton = () => save.style.display = existingPoints.size > 0 ? 'block' : 'none';
+
+    toggleSaveButton();
+    save.addEventListener('click', (event) => {
+        event.stopPropagation();
+        const title = document.getElementById('title').value || document.getElementById('title').placeholder
+        GeoJSONFeatures.saveAllPoints(title);
+    });
+
     const eTitle = document.getElementById('title');
     eTitle.value = eTitle.defaultValue;
 }
