edd617929cc45d2e660b052e4b98847f7f859405
Miguel Pagola <mipagola@gmail.com>
2023-06-12 12:51:43 +0200

    feat: a point cna have no image

--- js/point.js
+++ js/point.js
@@ -3,18 +3,20 @@ import { Card } from "./card.js";
 class Point {
     feature; card;
 
-    constructor({ image, geojson }) {
-        const id = image.name;
+    constructor({ image, label, geojson }) {
+        const id = image ? image.name : label;
 
         this.feature = JSON.parse(JSON.stringify(geojson));
         this.feature.id = id;
 
         const { data } = this.feature;
-        data.image = image;
+        if (image) {
+            data.image = image;
+        }
 
         this.feature.properties = {
             name: id,
-            description: 'A description about the image'
+            description: `A description about the ${image ? 'image' : 'point'}`
         };
 
         this.card = new Card(this.feature);
