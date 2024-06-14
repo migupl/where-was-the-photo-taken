87fcd5b6980bf550142ed39a95aa22061ec7c6c1
Miguel Pagola <mipagola@gmail.com>
2024-06-14 16:55:51 +0200

    refactor: duplicate code

--- js/point.js
+++ js/point.js
@@ -30,4 +30,30 @@ class Point {
     #label = latlng => `lat: ${latlng.lat}, lng: ${latlng.lng}`
 }
 
-export { Point }
+const point = (image, latlng, geojson) => {
+    const feature = JSON.parse(JSON.stringify(geojson));
+
+    if (image) {
+        feature.id = image.name
+        feature.data.image = image
+    } else {
+        feature.id = `lat: ${latlng.lat}, lng: ${latlng.lng}`
+    }
+
+    feature.properties = feature.properties || {}
+
+    const card = new Card(feature);
+
+    const has = f => feature.id == f.id;
+    const updatePopupWith = feature => card.updatePopup(feature);
+    const wasUpdated = () => card.wasUpdated();
+
+    return {
+        id: feature.id,
+        feature,
+        has,
+        updatePopupWith,
+        wasUpdated,
+    }
+};
+export { point as Point }
