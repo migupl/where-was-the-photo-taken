9fb87a44f9578bb3ce359121d07e1032aed808f5
Miguel Pagola <mipagola@gmail.com>
2024-06-14 18:45:01 +0200

    refactor: include 'card' into 'point' file

--- js/point.js
+++ js/point.js
@@ -1,5 +1,3 @@
-import { card } from "./card.js";
-
 const point = (image, latlng, geojson) => {
     const feature = JSON.parse(JSON.stringify(geojson));
 
@@ -11,11 +9,9 @@ const point = (image, latlng, geojson) => {
     }
 
     feature.properties = feature.properties || {}
-
+    const c = card(feature);
 
     const has = f => feature.id == f.id;
-
-    const c = card(feature);
     const updatePopupWith = feature => c.updatePopup(feature);
     const wasUpdated = c.wasUpdated;
 
@@ -27,4 +23,100 @@ const point = (image, latlng, geojson) => {
         wasUpdated,
     }
 };
+
+const card = features => {
+
+    const updatePopup = feature => {
+        const { name, description } = feature.properties;
+
+        const titleEl = popupEl.querySelector('input');
+        titleEl.value = name;
+
+        const descriptionEl = popupEl.querySelector('textarea');
+        descriptionEl.value = description;
+    }
+
+    const addDescriptionTo = cardEl => {
+        const { properties } = feature;
+
+        const description = cardEl.querySelector('textarea');
+        description.placeholder = 'A description about...';
+        if (properties.description) {
+            description.value = properties.description;
+        }
+        description.addEventListener('input', event => {
+            event.stopPropagation();
+
+            const { value: description } = event.target;
+            if(description) feature.properties.description = description
+            updated = true;
+        });
+    }
+
+    const addImageTo = cardEl => {
+        const { id, data: { image } } = feature;
+
+        if (image) {
+            const img = cardEl.querySelector('img');
+            img.src = URL.createObjectURL(image);
+            img.alt = id;
+        }
+    }
+
+    const addTitleTo = cardEl => {
+        const { id, properties } = feature;
+
+        const title = cardEl.querySelector('input');
+        title.placeholder = id;
+        if (properties.name) {
+            title.value = properties.name;
+        }
+        title.addEventListener('input', event => {
+            event.stopPropagation();
+
+            const { value: name } = event.target;
+            if (name) feature.properties.name = name
+            updated = true
+        });
+    }
+
+    const setPopup = () => {
+        const cardEl = document
+            .createRange()
+            .createContextualFragment(template);
+
+        addImageTo(cardEl);
+        addTitleTo(cardEl);
+        addDescriptionTo(cardEl);
+        setPopupContent(cardEl);
+    }
+
+    const setPopupContent = cardEl => {
+        popupEl = document.createElement('div');
+        popupEl.appendChild(cardEl);
+
+        const { properties } = feature;
+        properties.popupContent = popupEl;
+    }
+
+    const template = `
+<div>
+    <img src="" alt="">
+    <input type="text" name="title" id="card-title" placeholder="a title">
+    <textarea name="description" id="card-description"></textarea>
+</div>
+`;
+
+    const feature = features;
+    let popupEl;
+    let updated = false;
+
+    setPopup()
+
+    return {
+        updatePopup,
+        wasUpdated: updated
+    }
+};
+
 export { point as Point }
